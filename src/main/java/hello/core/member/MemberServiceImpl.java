ad2e6cc579bc34342abba098dde1e7766a966c55
sirzzang <sirzzang@naver.com>
2022-01-12 23:27:49 +0900

    member service implementation problem

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /*
+     * DIP 원칙 위반
+     * MemberServiceImpl은 MemberRepository 인터페이스 뿐만 아니라,
+     * MemberRepository 구현체에도 의존한다
+     */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
