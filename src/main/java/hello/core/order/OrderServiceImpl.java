9fa2687ffa9e2f6eefc44df488a117b5552f492a
sirzzang <sirzzang@naver.com>
2022-01-24 23:01:45 +0900

    violating DIP: OrderServiceImpl depending on DiscountPolicy instance

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,8 +10,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     // 회원, 할인 정책
+    /* 문제
+     - OCP, DIP를 지킨 것 같지만, 클라이언트(Service)가 구현 클래스에도 의존
+     - 할인 정책을 변경하려면, DiscountPolicy를 변경하려면 ServiceImpl의 코드를 고쳐야 함
+     */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
