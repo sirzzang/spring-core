da32337feef1d79c65937fc19810ca94e25eee89
sirzzang <sirzzang@naver.com>
2022-02-01 21:00:25 +0900

    AppConfig 코드 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,14 +13,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 출력 예상(순서 무관)
+ * @Configuration이 없을 때(순서 무관)
  * call AppConfig.memberService
  * call AppConfig.memberRepository
  * call AppConfig.memberRepository
  * call AppConfig.orderService
  * call AppConfig.memberRepository
  *
- * 실제 출력 결과
+ * @Configuration이 있을 때
  * call AppConfig.memberRepository
  * call AppConfig.memberService
  * call AppConfig.orderService
@@ -32,6 +32,10 @@ import org.springframework.context.annotation.Configuration;
  * 이 임의의 다른 클래스가 싱글톤 패턴을 보장하는 것이다.
  * - 이 클래스는 아마 AppConfig 내에 뭔가 등록되어 있는 게 있으면 찾아서 반환하고,
  * - 등록되어 있는 게 없으면 등록하는 방식으로?
+ * @Coinfiguration을 빼면, 싱글톤이 깨진다.
+ * - @Bean으로 등록된 것은 스프링 빈으로 등록되지만, CGLIB에 의해 바이트 코드가 조작되지 않는다.
+ * - @Bean으로 등록되지 않고 자바 코드 호출에 의해 생성된 객체는 스프링 컨테이너의 관리를 받는 빈이 아니다.
+ * - 이 경우에도 @Autowired를 사용하면 문제가 해결되기는 한다.
  */
 
 @Configuration
@@ -39,24 +43,24 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        System.out.println("call AppConfig.MemberRepository");
+//        System.out.println("call AppConfig.MemberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
     @Bean
     public MemberService memberService() {
-        System.out.println("call AppConfig.memberService");
+//        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
-        System.out.println("call AppConfig.orderService");
+//        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
