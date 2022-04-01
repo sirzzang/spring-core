bab0d2e70b49abe5ea8424d14942ff9550bc75d8
sirzzang <sirzzang@naver.com>
2022-04-02 00:37:52 +0900

    BeanLifecycle 노트 리팩토링, 생명주기 콜백 인터페이스 내용 추가

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,17 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     // 연결 객체 생성 및 연결, 호출 초기화
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect(); // 객체 생성 시 연결
-        call("초기화 연결 메시지"); // 서버 호출
+        // connect(); // 객체 생성 시 연결
+        // call("초기화 연결 메시지"); // 서버 호출
     }
 
     public void setUrl(String url) {
@@ -26,7 +29,22 @@ public class NetworkClient {
     }
 
     // 서비스 종료 시 연결 종료 메서드
-    public  void disconnect() {
+    public void disconnect() {
         System.out.println("disconnect 호출, close " + url);
+    }
+
+    // 의존관계 주입 후 호출할 메서드: 기존의 connect, call
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 소멸 직전 호출할 메서드
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
